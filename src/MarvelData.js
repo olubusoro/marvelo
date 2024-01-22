318be70dfb62f2f23b08cd380298a1edd3e530dd
Jude Iyanu <jude59270@gmail.com>
2024-01-22 15:05:39 +0100

    Final Project

---
+++ src/MarvelData.js
@@ -0,0 +1,91 @@
+export const characters = [
+  // Iron Man
+  {
+    name: "Lebron James",
+    alias: "The Goat",
+    img1: "https://media.gettyimages.com/id/1039547504/photo/2018-19-los-angeles-lakers-media-day.jpg?b=1&s=594x594&w=0&k=20&c=VQoe-UYfiyl61bUd9SkUxvu4og2Eyd2HewlKxQmSQYo=",
+    homeworld: "Earth",
+    abilities: ["Explosive Power Jumps", "Fast breaks", "Poster king"],
+    team: "Lakers",
+    description:
+      "Lebron James also known as the greatest of all times even though he doest deserve it is one of the best players the NBA hasa ever seen",
+  },
+  {
+    name: "steph curry",
+    alias: "Baby faced assasin",
+    abilities: ["Buzzer beater", "Chef", "Riduculously high arched shots"],
+    team: "Golden states",
+    img1: "https://heavy.com/wp-content/uploads/2020/07/GettyImages-970179654-e1594340962737.jpg?quality=65&strip=all",
+    homeworld: "Earth",
+    description:
+      "Steph Curry also known as the chef also known as the baby face assasin is one of the greatest shooters in the history of official basketball.",
+  },
+  {
+    name: "Kobe Bryant",
+    alias: "Black Mamba",
+    abilities: ["fade aways", "inside shots"],
+    team: "Lakers",
+    img1: "https://th.bing.com/th/id/R.6effe44ccf54e7817f680171ad6fbbbe?rik=1CiCnawmNaWj%2fA&riu=http%3a%2f%2fa.espncdn.com%2fphoto%2f2013%2f0429%2fmag_kobe_17.jpg&ehk=Fbtko4DRw45%2frMZgVOAC3%2fn5WHjkV%2bmtYMylKNybMoE%3d&risl=&pid=ImgRaw&r=0",
+    homeworld: "Earth",
+
+    description:
+      "Kobe Bryant also known as the balck mamba is one of the est players the NBA has ever seen, he is also known as a ball hog and in his oown words he said I CAN ONLY TRUST MYSELF WITH THE BALL",
+  },
+  {
+    name: "Micheal Jordan",
+    alias: "The real GOAT",
+    abilities: ["free throw line windmil dunk", "one handed ball pass fake"],
+    team: "chicago bulls",
+    img1: "https://th.bing.com/th/id/R.f041221982e0d15a3022711b79f9c384?rik=XVLf1RSQAnxOgQ&riu=http%3a%2f%2fcdn.pursuitist.com%2fwp-content%2fuploads%2f2015%2f05%2fmichael-jordan.jpg&ehk=W1gV4lZ6E9mEz1eV8CgtxVOUfVF1du5101sWCuMk3Tg%3d&risl=&pid=ImgRaw&r=0",
+    homeworld: "Earth",
+
+    description: "micheal jordan is the one and only true GOAT.",
+  },
+
+  // Spider-Man
+  {
+    name: "Allen Iverson",
+    alias: "shifty man",
+    abilities: ["Bio-electrokinesis", "Wall-crawling"],
+    team: "Champions",
+    img1: "https://th.bing.com/th/id/R.996f9385a06463bf82dd2dddce578d67?rik=GuJDArnphqONXA&pid=ImgRaw&r=0",
+    homeworld: "Earth",
+    img3: "url_to_img3_for_Natasha_Stark",
+    description:
+      "allen iverson is one of the most shifty players ever recorded in the NBA history.",
+  },
+
+  {
+    name: "Ja morant",
+    alias: "Dunk king",
+    abilities: ["Web-shooting", "Superhuman agility"],
+    team: "boston celtics",
+    img1: "https://th.bing.com/th/id/R.ff891b738c4f6cdbba1322fe7e7f8290?rik=RzoQiouqLD1muw&pid=ImgRaw&r=0",
+    homeworld: "Earth",
+    description:
+      "ja morant is a player known for his super jumps and hangtime on the air",
+  },
+
+  {
+    name: "russel westbrook",
+    alias: "dart",
+    abilities: ["fast offensive plays", "true zone"],
+    team: "Oaklhaoma city",
+    img1: "https://th.bing.com/th/id/R.b60b5ff01337e7483919160515c2a554?rik=E3%2bMoCZyj%2fweGw&pid=ImgRaw&r=0",
+    homeworld: "Earth",
+    description:
+      "russel westbrook is the fastest pointgaurd in the history of NBA",
+  },
+  {
+    name: "James harden",
+    alias: "stepping",
+    abilities: ["Euro step", "stepbacks"],
+    team: "Rockets",
+    img1: "https://th.bing.com/th/id/R.cc86ec3a9c3f9bb5fbfdb52d576f1d08?rik=kDbqZTksk7rj%2fA&pid=ImgRaw&r=0",
+    homeworld: "Earth",
+    description:
+      "James harden is a very aggressive player known for his notorious stepbacks which are very hard to guard",
+  },
+];
+
+console.log(characters);
